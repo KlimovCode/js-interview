bf3a8e5cd29d99f3be2a4a9a764a0691d772f4f3
KlimovCode <74239490+KlimovCode@users.noreply.github.com>
2020-12-07 19:02:38 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = 5002
+
+app.listen(PORT, () => console.log('Server has started on port ' + PORT))
