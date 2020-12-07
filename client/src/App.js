ae86b4d24430160e75f8fdaa16a3c08c705cebdd
klimovcode <klimovcode@mail.ru>
2020-12-08 00:05:55 +0500

    ported index page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+
+import { Footer } from './common/Footer'
+import { Header } from './common/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <Header />
+        <main>
+
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ client/src/common/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="footer fixed-bottom">
+      &copy; 2020 KlimovCode
+    </footer>
+  )
+}
